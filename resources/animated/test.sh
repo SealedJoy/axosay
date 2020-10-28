f1d131d656475b8a0417e1fb7a9b5c5e790d83bf
SealedJoy <sealedjoy@gmail.com>
2020-10-28 04:27:29 +0000

    upload

---
+++ resources/animated/test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+while true; do
+clear ; cat axolotl/axolotl1 ; sleep 0.3 
+clear ; cat axolotl/axolotl2 ; sleep 0.3 
+clear ; cat axolotl/axolotl3 ; sleep 0.3 
+clear ; cat axolotl/axolotl4 ; sleep 0.3 
+done
