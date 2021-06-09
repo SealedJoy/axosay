3d3e95956a20ec2486fd9f6652fb72cacb378b97
SealedJoy <sealedjoy@gmail.com>
2021-06-09 12:33:34 +0100

    add init octelpus frames

---
+++ resources/animated/octelpus_test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+while true; do
+clear ; cat octelpus/octelpus1 ; sleep 0.3 
+clear ; cat octelpus/octelpus2 ; sleep 0.3 
+clear ; cat octelpus/octelpus3 ; sleep 0.3 
+clear ; cat octelpus/octelpus4 ; sleep 0.3 
+done
